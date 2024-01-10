40e09bcb7aa5981be9e5f71e9ec250d98064c37f
tungl27 <tungluong2706@gmail.com>
2024-01-10 11:23:05 +0700

    Done sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SideBar from './components/Sidebar/UI';
+import BS_Report from './components/BS_Report/UI/BS_Report';
 function App() {
   return (
     <div className='flex flex-row'>
       <SideBar />
       <div className='bg-red-50 w-4/5'>
-        <h1>hiha</h1>
         <BrowserRouter>
           <Routes>
+            <Route path='/' element={BS_Report}/>
             <Route />
-            <Route ></Route>
             <Route >
               <Route />
               <Route />
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./Utils/output.css";
 // import InvoiceDetails from "./components/Invoice_Management/InvoiceDetails";
 // import Invoice_Management from "./components/Invoice_Management/Invoice_Managment.jsx";
 // import PL_Report from "./components/PL_Report/UI/PL_Report.jsx";
-import BS_Report from "./components/BS_Report/UI/BS_Report.jsx";
+// import BS_Report from "./components/BS_Report/UI/BS_Report.jsx";
 import { AppProvider } from "./Utils/contexts/app.context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -15,6 +15,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
   <AppProvider>
     {/* <Invoice_Management /> */}
-    <BS_Report/>
+    <App/>
   </AppProvider>
 );
