29ddff12e2aa87cd2a29f1258526be748ad54e44
SelinaNguyen98 <ntmnguyet@starboard-asia.com>
2024-01-19 14:03:57 +0700

    Push code

--- src/components/Login/Controller/index.js
+++ src/components/Login/Controller/index.js
@@ -6,7 +6,9 @@ export const loginUser = async (username, password) => {
   // eslint-disable-next-line no-useless-catch
   try {
     const response = await API.login(username, password);
+    console.log(response.user.id);
     localStorage.setItem('token',  response.access_token); 
+    localStorage.setItem('user_id',  response.user.id); 
     return response;
   } catch (error) {
     throw error;
