93fe39fa33b3b12b679a00a0ac7de07154256867
SelinaNguyen98 <ntmnguyet@starboard-asia.com>
2024-02-02 13:16:30 +0700

    Push code

--- src/components/Login/UI/Login.jsx
+++ src/components/Login/UI/Login.jsx
@@ -15,7 +15,7 @@ import classNames from "classnames";
 import { useTranslation } from "react-i18next";
 import { locales } from "../../../Utils/i18n/i18n";
 import { loginUser } from "../Controller";
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 function Login() {
   // eslint-disable-next-line no-unused-vars
   const { i18n, t } = useTranslation();
@@ -33,7 +33,7 @@ function Login() {
 
   const handleLogin = async () => {
     try {
-      if(!values.username && !values.password){
+      if (!values.username && !values.password) {
         setError("Please fill in your account information and password!");
         setTimeout(() => {
           setError("");
@@ -65,19 +65,24 @@ function Login() {
       setTimeout(() => {
         setError("");
       }, 3000);
-    } finally{
+    } finally {
       // setError("");
     }
   };
-
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      // Gọi hàm handleLogin khi phím Enter được nhấn
+      handleLogin();
+    }
+  };
   const [showPassword, setShowPassword] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword((prevShowPassword) => !prevShowPassword);
   };
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.removeItem("token");
-  },[])
+  }, []);
   return (
     <div>
       {/* Thanh header màu xanh */}
@@ -102,125 +107,137 @@ function Login() {
                 />
               </div>
               <Popover
-              className="flex items-center cursor-pointer ml-2"
-              renderPopover={
-                <div className=" bg-yellow rounded-[12px]">
-                  <div className="flex flex-col items-center justify-center px-4 py-2  ">
-                    <button
-                      className="flex items-center py-2 px-3 rounded-[16px] text-left font-bold text-sm uppercase bg-white w-[106px] justify-between"
-                      onClick={() => changeLanguage("en")}
-                    >
-                      <span>English</span>
-                      <span
-                        className={`ml-2 ${
-                          currentLanguage === "en" ? "" : "hidden"
-                        }`}
+                className="flex items-center cursor-pointer ml-2"
+                renderPopover={
+                  <div className=" bg-yellow rounded-[12px]">
+                    <div className="flex flex-col items-center justify-center px-4 py-2  ">
+                      <button
+                        className="flex items-center py-2 px-3 rounded-[16px] text-left font-bold text-sm uppercase bg-white w-[106px] justify-between"
+                        onClick={() => changeLanguage("en")}
                       >
-                        <svg
-                          viewBox="0 0 19 15"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="w-4 h-4"
+                        <span>English</span>
+                        <span
+                          className={`ml-2 ${
+                            currentLanguage === "en" ? "" : "hidden"
+                          }`}
                         >
-                          <path
-                            d="M2 8.6L6.28571 13L17 2"
-                            stroke="#3CA745"
-                            strokeWidth="4"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                      </span>
-                    </button>
+                          <svg
+                            viewBox="0 0 19 15"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-4 h-4"
+                          >
+                            <path
+                              d="M2 8.6L6.28571 13L17 2"
+                              stroke="#3CA745"
+                              strokeWidth="4"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />
+                          </svg>
+                        </span>
+                      </button>
 
-                    <button
-                      className="flex items-center py-2 px-3 mt-2 rounded-[16px] text-left font-bold text-sm uppercase bg-white w-full justify-between"
-                      onClick={() => changeLanguage("jp")}
-                    >
-                      <span className=""> 日本語</span>
-                      <span
-                        className={`ml-2 ${
-                          currentLanguage === "jp" ? "" : "hidden"
-                        }`}
+                      <button
+                        className="flex items-center py-2 px-3 mt-2 rounded-[16px] text-left font-bold text-sm uppercase bg-white w-full justify-between"
+                        onClick={() => changeLanguage("jp")}
                       >
-                        <svg
-                          viewBox="0 0 19 15"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="w-4 h-4"
+                        <span className=""> 日本語</span>
+                        <span
+                          className={`ml-2 ${
+                            currentLanguage === "jp" ? "" : "hidden"
+                          }`}
                         >
-                          <path
-                            d="M2 8.6L6.28571 13L17 2"
-                            stroke="#3CA745"
-                            strokeWidth="4"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          />
-                        </svg>
-                      </span>
-                    </button>
+                          <svg
+                            viewBox="0 0 19 15"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-4 h-4"
+                          >
+                            <path
+                              d="M2 8.6L6.28571 13L17 2"
+                              stroke="#3CA745"
+                              strokeWidth="4"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />
+                          </svg>
+                        </span>
+                      </button>
+                    </div>
                   </div>
-                </div>
-              }
-            >
-              <PiTranslateFill
-                className="w-10 h-10 cursor-pointer flex-shrink-0"
-                // onClick={handleLanguageSwitch}
-              />
-            </Popover>
+                }
+              >
+                <PiTranslateFill
+                  className="w-10 h-10 cursor-pointer flex-shrink-0"
+                  // onClick={handleLanguageSwitch}
+                />
+              </Popover>
             </div>
           </div>
           {/* Hộp thoại đăng nhập */}
           <div className="absolute w-96 bg-[#16205e] p-4 rounded-3xl z-50 mt-20 shadow-2xl ">
             <h1 className="text-[#e23d31] font-bold text-xl text-center mb-4">
-            {t("Login.Welcome")}
+              {t("Login.Welcome")}
             </h1>
             <div className="text-center text-sm font-bold mb-4 text-white animate-pulse">
-            {t("Login.Login")}
+              {t("Login.Login")}
             </div>
             <div className="mb-4">
               <label htmlFor="email" className="block text-white mb-2">
-              {t("Login.Account")}
+                {t("Login.Account")}
               </label>
               <input
-            type="text"
-            id="username"
-            className="w-full border rounded-3xl p-2 bg-white text-[#16205e] placeholder-[#16205e]::placeholder"
-            value={values.username}
-            onChange={(e) => setValues({ ...values, username: e.target.value })}
-            placeholder={t("Login.placeHolder_Account")}
-          />
+                type="text"
+                id="username"
+                className="w-full border rounded-3xl p-2 bg-white text-[#16205e] placeholder-[#16205e]::placeholder"
+                value={values.username}
+                onChange={(e) =>
+                  setValues({ ...values, username: e.target.value })
+                }
+                onKeyPress={handleKeyPress} 
+                placeholder={t("Login.placeHolder_Account")}
+              />
             </div>
             <div className="mb-4">
               <label htmlFor="password" className="block text-white mb-2">
-              {t("Login.Password")}
+                {t("Login.Password")}
               </label>
               <div className="relative">
-          <input
-            type={showPassword ? 'text' : 'password'}
-            id="password"
-            className="w-full border rounded-3xl p-2 bg-white text-[#16205e] placeholder-[#16205e]::placeholder"
-            value={values.password}
-            onChange={(e) => setValues({ ...values, password: e.target.value })}
-            placeholder={t("Login.placeHolder_Password")}
-          />
-          <div
-            className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
-            onClick={togglePasswordVisibility}
-          >
-            {showPassword ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
-          </div>
-        </div>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  className="w-full border rounded-3xl p-2 bg-white text-[#16205e] placeholder-[#16205e]::placeholder"
+                  value={values.password}
+                  onChange={(e) =>
+                    setValues({ ...values, password: e.target.value })
+                  }
+                  onKeyPress={handleKeyPress} 
+                  placeholder={t("Login.placeHolder_Password")}
+                />
+                <div
+                  className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
+                  onClick={togglePasswordVisibility}
+                >
+                  {showPassword ? (
+                    <FaEye size={20} />
+                  ) : (
+                    <FaEyeSlash size={20} />
+                  )}
+                </div>
+              </div>
             </div>
-            <div className="text-red-500 mb-4 text-[12px] min-h-2 text-center leading-5">{error}</div>
+            <div className="text-red-500 mb-4 text-[12px] min-h-2 text-center leading-5">
+              {error}
+            </div>
 
             <div className="flex justify-center">
-            <button
-            className="bg-[#e23d31] text-white rounded-full px-24 py-2"
-            onClick={handleLogin}
-          >
-            {t("button.Login")}
-          </button>
+              <button
+                className="bg-[#e23d31] text-white rounded-full px-24 py-2"
+                onClick={handleLogin}
+              >
+                {t("button.Login")}
+              </button>
             </div>
           </div>
         </div>
